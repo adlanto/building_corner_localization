84093cc5b073f0243aeb64ce8dba125aa39511fb
Volker <volker.manz@stud.hs-kempten.de>
2020-04-23 18:54:50 +0200

    New parameters in the functions

--- preprocessing.py
+++ preprocessing.py
@@ -6,10 +6,10 @@ def process_frame(frame: np.ndarray) -> np.ndarray:
 
     # Volker Anfang: Beliebige Filter einsetzen
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    crop = frame[0:230]
+    crop = gray[0:230]
     print(crop.shape)
     # Variante 1:
-    blur = cv2.GaussianBlur(crop,(5,5),0)
+    blur = cv2.GaussianBlur(gray,(7,7),0)
     ret, thresh = cv2.threshold(blur, 90, 255, cv2.THRESH_TOZERO_INV)
     #ret, thresh = cv2.adaptiveThreshold(blur, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY,15,12)
     cv2.imshow('blurred + threshed', thresh)
