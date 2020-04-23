6a7641102beacf92eb5273c29b6c854ea2852a6d
Volker <volker.manz@stud.hs-kempten.de>
2020-04-23 18:48:31 +0200

    New parameters in the functions

--- preprocessing.py
+++ preprocessing.py
@@ -6,28 +6,19 @@ def process_frame(frame: np.ndarray) -> np.ndarray:
 
     # Volker Anfang: Beliebige Filter einsetzen
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    crop = frame[0:230]
+    print(crop.shape)
     # Variante 1:
-    #blur = cv2.GaussianBlur(gray,(5,5),0)
-    #ret, thresh = cv2.threshold(blur, 180, 255, cv2.THRESH_TOZERO_INV)
-    #cv2.imshow('blurred', blur)
+    blur = cv2.GaussianBlur(crop,(5,5),0)
+    ret, thresh = cv2.threshold(blur, 90, 255, cv2.THRESH_TOZERO_INV)
+    #ret, thresh = cv2.adaptiveThreshold(blur, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY,15,12)
+    cv2.imshow('blurred + threshed', thresh)
 
     # Variante 2:
-    F1 = np.array([[1, 1, 1],
-                   [0, 0, 0],
-                   [-1, -1, -1]])
-
-    F2 = np.array([[1, 0, -1],
-                   [1, 0, -1],
-                   [1, 0, -1]])
-    X1 = cv2.filter2D(gray, -1, F1)
-    X2 = cv2.filter2D(gray, -1, F2)
-    x_mag = np.sqrt(X1 ** 2 + X2 ** 2)
-    X_mag = np.uint8(np.abs(x_mag) / (3 * 255.0))
-    print(X_mag.shape)
-    cv2.imshow('X_mag', X_mag)
 
 
-    return_image = X_mag
+
+    return_image = thresh
     # Volker Manz Ende
     # Ziel: möglichst viele Punkte am Rande von den Gebäuden / auch braunes rechts
 
