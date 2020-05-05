7e7823cfb46aba8cc803f3af6bf413991a8c35e4
Volker <volker.manz@stud.hs-kempten.de>
2020-05-05 12:44:30 +0200

    Treshhold of Point Pair

--- stereoTriangulation.py
+++ stereoTriangulation.py
@@ -1,5 +1,4 @@
 import numpy as np
-import math
 
 
 ################################# Beispielwerte #######################################
@@ -11,14 +10,14 @@ grad1= 25 / 360 * 2 * np.pi
 grad2= 17 / 360 * 2 * np.pi
 
 # Distanz aus Sicht der einzelnen Kameras
-dk1 = x1 / math.sin(grad1)
-dk2 = x2 / math.sin(grad2)
+dk1 = x1 / np.sin(grad1)
+dk2 = x2 / np.sin(grad2)
 
 ##########################################################################################
 
 # Distanz berechnen
-y1 = x1 / math.tan(grad1)
-y2 = x2 / math.tan(grad2)
+y1 = x1 / np.tan(grad1)
+y2 = x2 / np.tan(grad2)
 
 # Abstand der einzelnen Kameras von dem Weltkoordinatemittelpunkt
 xk1 = -0.5
@@ -34,8 +33,8 @@ Pk2 = [xw2, (y2)]
 
 # Ausgabe des Punktepaars
 print()
-print(Pk1)
-print(Pk2)
+print('Pk1 =', Pk1)
+print('Pk2 =', Pk2)
 
 #Schwellwerte x
 # 5% Toleranz
@@ -47,30 +46,31 @@ x12 = xw2 - (xw2 * thresh)
 x22 = (xw2 * thresh) + xw2
 
 print()
-print(x11)
-print(x21)
+print('x11 =', x11)
+print('x21 =', x21)
 print()
-print(x12)
-print(x22)
+print('x12 =', x12)
+print('x22 =', x22)
 
 #Schwellwerte y
 # 5% Toleranz
 thresh = 0.05
-y11 = y1 -(y1 * thresh)
+y11 = y1 - (y1 * thresh)
 y21 = (y1 * thresh) + y1
 
 y12 = y2 - (y2 * thresh)
 y22 = (y2 * thresh) + y2
 
 print()
-print(y11)
-print(y21)
+print('y12 =', y11)
+print('y21 =', y21)
 print()
-print(y12)
-print(y22)
+print('y12 =', y12)
+print('y22 =', y22)
 
 # Prüfung ob Kamera 1 den selben Punkt wie Kamera 2 als Kantenpunkt entdeckt hat
 if ((x11 <= xw1) and (xw1 <= x21)) == ((x12 <= xw2) and (xw2 <= x22)):
+    print()
     print('Point in x match')
 
     if ((y11 <= y1) and (y1 <= y21)) == ((y12 <= y2) and (y2 <= y22)):
@@ -80,5 +80,3 @@ if ((x11 <= xw1) and (xw1 <= x21)) == ((x12 <= xw2) and (xw2 <= x22)):
 
 else:
     print('Error in Point x')
-
-
