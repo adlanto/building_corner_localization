fd5a18a713c0b730f8e5f05294a92ac20899f5f0
Volker <volker.manz@stud.hs-kempten.de>
2020-05-05 12:29:20 +0200

    Treshhold of Point Pair

--- stereoTriangulation.py
+++ stereoTriangulation.py
@@ -29,17 +29,56 @@ xw1 = x1 + xk1
 xw2 = x2 + xk2
 
 # Punkte in WEltkoordinaten
-Pk1 = [xw1, math.ceil(y1)]
-Pk2 = [xw2, math.ceil(y2)]
+Pk1 = [xw1, (y1)]
+Pk2 = [xw2, (y2)]
 
 # Ausgabe des Punktepaars
+print()
 print(Pk1)
 print(Pk2)
 
-# Prüfung ob Kamera 1 den selbenPunkt wie Kamera 2 als Kantenpunkt entdeckt hat
-if Pk1 == Pk2:
-    print('Point match')
+#Schwellwerte x
+# 5% Toleranz
+thresh = 0.05
+x11 = xw1 -(xw1 * thresh)
+x21 = (xw1 * thresh) + xw1
+
+x12 = xw2 - (xw2 * thresh)
+x22 = (xw2 * thresh) + xw2
+
+print()
+print(x11)
+print(x21)
+print()
+print(x12)
+print(x22)
+
+#Schwellwerte y
+# 5% Toleranz
+thresh = 0.05
+y11 = y1 -(y1 * thresh)
+y21 = (y1 * thresh) + y1
+
+y12 = y2 - (y2 * thresh)
+y22 = (y2 * thresh) + y2
+
+print()
+print(y11)
+print(y21)
+print()
+print(y12)
+print(y22)
+
+# Prüfung ob Kamera 1 den selben Punkt wie Kamera 2 als Kantenpunkt entdeckt hat
+if ((x11 <= xw1) and (xw1 <= x21)) == ((x12 <= xw2) and (xw2 <= x22)):
+    print('Point in x match')
+
+    if ((y11 <= y1) and (y1 <= y21)) == ((y12 <= y2) and (y2 <= y22)):
+        print('Point in y match')
+    else:
+        print('Error in Point y')
 
 else:
-    print('error')
+    print('Error in Point x')
+
 
