de8d6ce944c27a2aa956ffe68ebb8c3538100de1
adlanto <tobyl@t-online.de>
2020-05-12 08:34:15 +0200

    Updated stereo camera distance calculation

--- estimate_distances.py
+++ estimate_distances.py
@@ -6,4 +6,18 @@ def estimate_distances(building_corners_left, building_corners_right):
 
     distances = {}
 
+    # Parameter
+    f = 5 * 10 ^ -3  # Brennweite
+    x = 1  # Abstand der beiden Kameras
+    p = 3.75 * 10 ^ -6  # Pixelgröße
+
+    # Calculation of the distance
+    xl =  0# Pixelanzahl der linken Kamera
+    xr =  0# Pixelanzahl der rechten Kamera
+    xr =  0# Pixelanzahl der rechten Kamera
+
+    for i in range(1, np.array + 1):
+        d = (f * x) / (xl - xr)
+        print(d)
+
     return distances
