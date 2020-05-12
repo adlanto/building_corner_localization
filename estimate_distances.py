0a12b6d562c677c3bcfb712a66b2f6e2ff77df4a
Volker <volker.manz@stud.hs-kempten.de>
2020-05-12 08:26:47 +0200

    New calculation framework

---
+++ estimate_distances.py
@@ -0,0 +1,20 @@
+import numpy as np
+
+# Eingabe = Punkte-Array
+
+
+# Parameter
+f = 5*10^-3 # Brennweite
+x = 1 # Abstand der beiden Kameras
+p = 3.75 * 10^-6 # Pixelgröße
+
+# Calculation of the distance
+xl = # Pixelanzahl der linken Kamera
+xr = # Pixelanzahl der rechten Kamera
+
+for i in range(1, np.array+1):
+    d = (f * x)/(xl-xr)
+    print(d)
+
+
+
