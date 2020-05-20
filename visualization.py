d702406274ef2bc6764428fc1eb9c383df7eeede
adlanto <tobyl@t-online.de>
2020-05-20 23:59:15 +0200

    added birds_eye_map in visualization.py

--- visualization.py
+++ visualization.py
@@ -90,7 +90,93 @@ def hough_lines_visualization(image: np.ndarray, contours, contours_poly, lines,
     return True
 
 
-def birds_eye_map(relative_distances):
+def birds_eye_map(x_array, y_array):
+
+    # All variables in meter - scale for displaying an image in a higher resolution
+    scale = 10
+    map_size_x = 100
+    map_size_y = 100
+    ego_x = 50
+    ego_y = 100
+    ego_width = 2
+    camera_distance = 1
+    camera_fov = 90
+    y_camera_range = 100
+
+    map = np.ones((map_size_x * scale, map_size_y * scale, 3))
+
+    # Draw Ego Vehicle
+    left_upper_ego_point = (int((ego_x - ego_width / 2) * scale), int((ego_y - 1) * scale))
+    right_bottom_ego_point = (int((ego_x + ego_width / 2) * scale), int(ego_y * scale))
+    cv2.rectangle(map, left_upper_ego_point, right_bottom_ego_point, (0, 0, 0), -1)
+
+    # Draw Camera Centers on Ego Vehicle
+    camera_left_center = (int((ego_x - camera_distance / 2) * scale), int((ego_y - 1) * scale))
+    cv2.circle(map, camera_left_center, 2, (0, 0, 255), 2)
+    camera_right_center = (int((ego_x + camera_distance / 2) * scale), int((ego_y - 1) * scale))
+    cv2.circle(map, camera_right_center, 2, (0, 255, 0), 2)
+
+    # ** Draw Field of View Cones **
+
+    # Create Overlay for Cones
+    cone_overlay_right = np.ones((map_size_x * scale, map_size_y * scale, 3))
+    cone_overlay_left = np.ones((map_size_x * scale, map_size_y * scale, 3))
+
+    # Recalculate Variables based on Scale
+    camera_fov = np.pi * camera_fov / 180
+    x_camera_range = y_camera_range * np.tan(camera_fov / 2)
+    x_camera_range = x_camera_range * scale
+    y_camera_range = y_camera_range * scale
+    map_size_y = map_size_y * scale
+
+    # Cone of Left Camera
+    camera_left_cone_left_pt = np.array([int(camera_left_center[0] - x_camera_range / 2), map_size_y - y_camera_range])
+    camera_left_cone_right_pt = np.array([int(camera_left_center[0] + x_camera_range / 2), map_size_y - y_camera_range])
+    left_cone = np.array([[camera_left_center, camera_left_cone_left_pt, camera_left_cone_right_pt]], dtype=np.int32)
+    cv2.fillPoly(cone_overlay_left, left_cone, color=(0, 0, 255))
+
+    # Cone of Right Camera
+    camera_right_cone_left_pt = (int(camera_right_center[0] - x_camera_range / 2), map_size_y - y_camera_range)
+    camera_right_cone_right_pt = (int(camera_right_center[0] + x_camera_range / 2), map_size_y - y_camera_range)
+    right_cone = np.array([[camera_right_center, camera_right_cone_left_pt, camera_right_cone_right_pt]],
+                          dtype=np.int32)
+    cv2.fillPoly(cone_overlay_right, right_cone, color=(0, 255, 0))
+
+    # Add weighted Overlays to the original map
+    cv2.addWeighted(cone_overlay_left, 0.3, map, 1 - 0.3, 0, map)
+    cv2.addWeighted(cone_overlay_right, 0.3, map, 1 - 0.3, 0, map)
+
+    # ** Draw Points of Building Corner Localization **
+
+    # Iterate over all points
+    for x, y in zip(x_array, y_array):
+        x_text = 'x: ' + str(np.round(x, 2))
+        y_text = 'y: ' + str(np.round(y, 2))
+        x = x * scale
+        y = y * scale
+        # Draw every Point as Circle on the Map
+        cv2.circle(map, (int(x), int(y)), radius=int(scale / 2), color=(0, 0, 0), thickness=-1)
+        # Write coordinates of the Point next to it on the Map
+        cv2.putText(map, x_text, (int(x + scale), int(y - scale)), cv2.FONT_ITALIC, 0.05 * scale, (0, 0, 0), 1)
+        cv2.putText(map, y_text, (int(x + scale), int(y + scale)), cv2.FONT_ITALIC, 0.05 * scale, (0, 0, 0), 1)
+
+    # ** Other Information **
+
+    # Legend
+    cv2.line(map, (10, map_size_y - 80), (20, map_size_y - 80), color=(0, 0, 255), thickness=5)
+    cv2.putText(map, 'Left Camera', (25, map_size_y - 75), cv2.FONT_ITALIC, 0.05 * scale, (0, 0, 0), 1)
+    cv2.line(map, (10, map_size_y - 50), (20, map_size_y - 50), color=(0, 255, 0), thickness=5)
+    cv2.putText(map, 'Right Camera', (25, map_size_y - 45), cv2.FONT_ITALIC, 0.05 * scale, (0, 0, 0), 1)
+    cv2.line(map, (10, map_size_y - 20), (20, map_size_y - 20), color=(0, 255, 255), thickness=5)
+    cv2.putText(map, 'Camera Overlap', (25, map_size_y - 15), cv2.FONT_ITALIC, 0.05 * scale, (0, 0, 0), 1)
+
+    # Status Information
+    status_text = 'Number of detected building corners: ' + str(len(x_array))
+    cv2.putText(map, status_text, (10, 20), cv2.FONT_ITALIC, 0.05 * scale, (0, 0, 0), 1)
+
+
+    # ***** SHOW MAP *****
+    cv2.imshow('Birds-Eye-View Map', map)
 
     return
 
