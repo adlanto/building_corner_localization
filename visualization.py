e893e83cf4b6c67ceea38e93934115320a909e52
adlanto <tobyl@t-online.de>
2020-06-20 15:09:12 +0200

    updated visualization of birds eye map, stereo cameras are plotted in the same window now

--- visualization.py
+++ visualization.py
@@ -10,9 +10,10 @@ def building_corner_visualization(frame, building_corners, name):
         for x1, y1, x2, y2 in building_corner:
             cv2.line(frame, (x1, y1), (x2, y2), (0, 255, 0), 2)
 
-    cv2.imshow('Building Corners '+name, frame)
+    if PM.DEBUG_VISUALIZATION:
+        cv2.imshow('Building Corners '+name, frame)
 
-    return
+    return frame
 
 
 def debug_visualization(frame, preprocessed_frame, keypoints, hough_lines,
@@ -91,7 +92,7 @@ def hough_lines_visualization(image: np.ndarray, contours, contours_poly, lines,
     return True
 
 
-def birds_eye_map(x_array, y_array):
+def birds_eye_map(x_array, y_array, left_frame_with_corners, right_frame_with_corners):
 
     # All variables in meter - scale for displaying an image in a higher resolution
     scale = 10
@@ -104,18 +105,18 @@ def birds_eye_map(x_array, y_array):
     camera_fov = PM.CAMERA_FOV
     y_camera_range = 100
 
-    map = np.ones((map_size_x * scale, map_size_y * scale, 3))
+    map = np.full((map_size_x * scale, map_size_y * scale, 3), (62, 117, 59), dtype=np.uint8)
 
     # ** Draw lines **
     left_line_x = (map_size_x / 2 - 5.25) * scale
     right_line_x = (map_size_x / 2 + 1.75) * scale
     middle_line_x = (map_size_x / 2 - 1.75) * scale
-    cv2.line(map, (int(left_line_x), 0), (int(left_line_x), map_size_y * scale), color=(0.3, 0.3, 0.3), thickness=2)
-    cv2.line(map, (int(right_line_x), 0), (int(right_line_x), map_size_y * scale), color=(0.3, 0.3, 0.3), thickness=2)
+    cv2.line(map, (int(left_line_x), 0), (int(left_line_x), map_size_y * scale), color=(80, 80, 80), thickness=2)
+    cv2.line(map, (int(right_line_x), 0), (int(right_line_x), map_size_y * scale), color=(80, 80, 80), thickness=2)
     lane = np.array([[(int(left_line_x), 0), (int(left_line_x), map_size_y * scale),
                       (int(right_line_x), map_size_y * scale), (int(right_line_x), 0)]], dtype=np.int32)
-    cv2.fillPoly(map, lane, color=(0.6, 0.6, 0.6))
-    cv2.line(map, (int(middle_line_x), 0), (int(middle_line_x), map_size_y * scale), color=(1, 1, 1), thickness=1)
+    cv2.fillPoly(map, lane, color=(150, 150, 150))
+    cv2.line(map, (int(middle_line_x), 0), (int(middle_line_x), map_size_y * scale), color=(255, 255, 255), thickness=1)
 
     # Draw Ego Vehicle
     left_upper_ego_point = (int((ego_x - ego_width / 2) * scale), int((ego_y - 1) * scale))
@@ -131,8 +132,8 @@ def birds_eye_map(x_array, y_array):
     # ** Draw Field of View Cones **
 
     # Create Overlay for Cones
-    cone_overlay_right = np.ones((map_size_x * scale, map_size_y * scale, 3))
-    cone_overlay_left = np.ones((map_size_x * scale, map_size_y * scale, 3))
+    cone_overlay_right = np.full((map_size_x * scale, map_size_y * scale, 3), 255, dtype=np.uint8)
+    cone_overlay_left = np.full((map_size_x * scale, map_size_y * scale, 3), 255, dtype=np.uint8)
 
     # Recalculate Variables based on Scale
     camera_fov = np.pi * camera_fov / 180
@@ -174,22 +175,41 @@ def birds_eye_map(x_array, y_array):
 
     # ** Other Information **
 
-    # Legend
-    cv2.line(map, (10, map_size_y - 80), (20, map_size_y - 80), color=(0, 0, 255), thickness=5)
+    # Legend 62, 117, 59
+    cv2.line(map, (10, map_size_y - 80), (20, map_size_y - 80), color=(40, 80, 215), thickness=5)
     cv2.putText(map, 'Left Camera', (25, map_size_y - 75), cv2.FONT_ITALIC, 0.05 * scale, (0, 0, 0), 1)
-    cv2.line(map, (10, map_size_y - 50), (20, map_size_y - 50), color=(0, 255, 0), thickness=5)
+    cv2.line(map, (10, map_size_y - 50), (20, map_size_y - 50), color=(40, 185, 40), thickness=5)
     cv2.putText(map, 'Right Camera', (25, map_size_y - 45), cv2.FONT_ITALIC, 0.05 * scale, (0, 0, 0), 1)
-    cv2.line(map, (10, map_size_y - 20), (20, map_size_y - 20), color=(0, 255, 255), thickness=5)
+    cv2.line(map, (10, map_size_y - 20), (20, map_size_y - 20), color=(83, 134, 30), thickness=5)
     cv2.putText(map, 'Camera Overlap', (25, map_size_y - 15), cv2.FONT_ITALIC, 0.05 * scale, (0, 0, 0), 1)
 
     # Status Information
     status_text = 'Number of detected building corners: ' + str(len(x_array))
     cv2.putText(map, status_text, (10, 20), cv2.FONT_ITALIC, 0.05 * scale, (0, 0, 0), 2)
 
+    # ***** Create visualization image *****
 
-    # ***** SHOW MAP *****
-    cv2.imshow('Birds-Eye-View Map', map)
+    # Combine Stereo Camera images
+    cv2.putText(left_frame_with_corners, 'Left Camera', (10, 30), cv2.FONT_ITALIC, 0.1 * scale, (0, 255, 255), 2)
+    cv2.putText(right_frame_with_corners, 'Right Camera', (10, 30), cv2.FONT_ITALIC, 0.1 * scale, (0, 255, 255), 2)
+    difference = int(map.shape[0]) - (int(left_frame_with_corners.shape[0]) * 2)
 
-    return
+    # Create border image to plot between birds eye map and stereo camera images
+    height = int(difference / 3)
+    border_vertical = np.full((height, left_frame_with_corners.shape[1], 3), (159, 187, 160), dtype=np.uint8)
+    stereo_camera_images = cv2.vconcat([border_vertical, left_frame_with_corners, border_vertical,
+                                        right_frame_with_corners, border_vertical])
+    # cv2.imshow("stereo_camera_images", stereo_camera_images)
+    border_horizontal = np.full((map.shape[0], 60, 3), (159, 187, 160), dtype=np.uint8)
+    scaler = map.shape[1] / stereo_camera_images.shape[1]
+    # print(map.shape[1], stereo_camera_images.shape[0], scaler)
+    stereo_camera_images = cv2.resize(stereo_camera_images, (stereo_camera_images.shape[1], map.shape[0]))
+
+    # Bring all together and show it
+    vis_image = cv2.hconcat([border_horizontal, stereo_camera_images, border_horizontal, map, border_horizontal])
+    # cv2.imshow('Birds-Eye-View Map', map)
+    cv2.imshow('Building Corner Localization', vis_image)
+
+    return vis_image
 
 
